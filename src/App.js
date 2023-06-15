3c7ca41049ce7f860262bd09ce82338b0a59809e
Alejandro Pacheco <ale_pach03@hotmail.com>
2023-06-15 14:00:46 -0700

    Modified structure

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
-
+import React, {Component} from 'react';
 import './App.css';
+import GameBoard from './Components/GameBoard';
+import Input from './Components/Input';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h3> Sudoku Solver </h3>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <h1> Sudoku With Solver</h1>
+          <Input></Input>
+          <GameBoard></GameBoard>
+          
+        
+        
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Components/Input.js
@@ -0,0 +1,17 @@
+import React from "react"
+import './styles.css';
+
+function Input(){
+    
+
+    return (
+        <div className = "ButtonContainer">
+            <button className = "buttonStyle"> Easy </button>
+            <button className = "buttonStyle"> Medium </button>
+            <button className = "buttonStyle"> Hard </button>
+        </div>
+    );
+}
+
+export default Input;
+
