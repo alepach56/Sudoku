243382b4806866e13a7e63a68233443f62a03066
Alejandro Pacheco <ale_pach03@hotmail.com>
2023-06-16 23:25:57 -0700

    Functionality to add numbers to the screen when inputted

--- src/Components/GameBoard/GameBoard.js
+++ src/Components/GameBoard/GameBoard.js
@@ -3,33 +3,42 @@ import './GameBoard.css';
 import GridCell from '../GridCell/GridCell';
 
 function GameBoard() {
-  const [grid, setGrid] = useState(Array(9).fill(Array(9).fill(null)));
+    console.log('GameBoard received events');
+    const [grid, setGrid] = useState(Array(9).fill(Array(9).fill(null)));
 
-  const handleCellClick = (row, col) => {
-    // Handle cell click logic here
-  };
-
-  const handleKeyDown = (row, col, e) => {
-    // Handle keydown logic here
-  };
-
-  return (
-        <div className="board-grid">
+  
+    return (
+        <div className="board-grid" tabIndex={0}>
             {grid.map((row, rowIndex) => (
             <div key={`row-${rowIndex}`} className="grid-row">
             {row.map((cellValue, colIndex) => (
-                <GridCell
+                <GridCell row = {rowIndex} col = {colIndex}
                 key={`cell-${rowIndex}-${colIndex}`}
                 value={cellValue}
-                onClick={() => handleCellClick(rowIndex, colIndex)}
-                onKeyDown={(e) => handleKeyDown(rowIndex, colIndex, e)}
+              //  onClick={() => handleCellClick(rowIndex, colIndex)}
+              //  onKeyDown={(e) => handleKeyDown(rowIndex, colIndex, e)}
+                
                 />
             ))}
             </div>
         ))}
         </div>
     
-  );
+    );
 }
 
 export default GameBoard;
+
+/*
+  const handleKeyDown = (row, col, e) => {
+    console.log('User pressed: ');
+    if (e >= 0 && e < 10){
+        console.log(e);
+    }
+  };
+
+  const handleCellClick = (row, col) => {
+    // Handle cell click logic here
+    
+  };
+*/
--- src/Components/GridCell/GridCell.js
+++ src/Components/GridCell/GridCell.js
@@ -1,24 +1,42 @@
 import React, { useState } from 'react';
 import './GridCell.css';
 
-function GridCell() {
-  const [clicked, setClicked] = useState(false);
+function GridCell({row, col, val, onClick}) {
+    
+    const [clicked, setClicked] = useState(false);
+    const[number, setNumber] = useState(null);
 
-  const handleClick = () => {
-    setClicked(true);
-  };
+    const handleCellClick = () => {
+        console.log('GridCell received events');
+        setClicked(true);
+    };
 
-  const handleBlur = () => {
-    setClicked(false);
-  };
+    const handleBlur = () => {
+        setClicked(false);
+    };
 
-  return (
-    <div
-      className={`grid-cell ${clicked ? 'clicked' : ''}`}
-      onClick={handleClick}
-      onBlur={handleBlur}
-      tabIndex={0}
-    ></div>
+    
+
+    const handleKeyDown = event => {
+        if(event.keyCode == 8 || event.keyCode == 46){
+            setNumber(null);
+        }
+        const num = Number(event.key);
+        if (!isNaN(num) && num < 10 && num >=0){
+            setNumber(num);
+        }
+    };
+
+    return (
+        <div
+            className={`grid-cell ${clicked ? 'clicked' : ''}`}
+            onBlur={handleBlur}
+            tabIndex={0}
+            onClick ={event => handleCellClick(event)}
+            onKeyDown = {handleKeyDown}
+        >
+            {number !== null && <span className= "inputtedNumber">{number} </span>}
+        </div>
   );
 }
 
